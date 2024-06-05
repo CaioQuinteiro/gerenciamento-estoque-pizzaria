89c4a53603d363bde4de0d3467d92bcaa81702b9
CaioQuinteiro <caioquinteiro7@gmail.com>
2024-06-05 15:35:26 -0300

    Atualizacao DB Produtos

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=50)
+    qtde_estoque = models.IntegerField()
+    qtde_min = models.IntegerField()
+    data_validade = models.DateField()
+
+    def __str__(self) -> str:
+        return self.nome
+
--- produtos/views.py
+++ produtos/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Produto
 
 def produtos(request):
-    return render(request, 'produtos.html')
+    if request.method == "GET":
+        return render(request, 'produtos.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        qtde_estoque = request.POST.get('qtde_estoque')
+        qtde_min = request.POST.get('qtde_min')
+        data_validade = request.POST.get('data_validade')
+
+        produto = Produto(
+            nome = nome,
+            qtde_estoque = qtde_estoque,
+            qtde_min = qtde_min,
+            data_validade = data_validade
+        )
+
+        produto.save()
+
+        return HttpResponse('teste')
