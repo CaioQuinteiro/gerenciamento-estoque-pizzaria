2e9642a36f9905fd76e1922b1d57913068923700
CaioQuinteiro <caioquinteiro7@gmail.com>
2024-06-11 19:00:23 -0300

    Atualizaão css e html

--- runserver.py
+++ runserver.py
@@ -1,10 +1,12 @@
 import os
 import webbrowser
+import subprocess
 from threading import Timer
 
 def open_browser():
-    webbrowser.open_new("http://127.0.0.1:8000/auth/login")
+    webbrowser.open_new("http://127.0.0.1:8000/auth/login/")
 
 if __name__ == "__main__":
     Timer(1, open_browser).start()
-    os.system("python manage.py runserver")
+
+    subprocess.Popen(["python", "manage.py", "runserver"])
